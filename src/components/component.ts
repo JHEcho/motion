696b25287d8242fdf9efca277c231f38e2e528f9
JHEcho <pyleso@nate.com>
2021-04-09 00:15:12 +0900

    Drap & Drop: Working but ugly

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,6 +1,7 @@
 export interface Component {
   attachTo(parent: HTMLElement, position?: InsertPosition): void;
   removeFrom(parent: HTMLElement): void;
+  attach(component: Component, position?: InsertPosition): void;
 }
 
 /**
@@ -8,11 +9,13 @@ export interface Component {
  */
 export class BaseComponent<T extends HTMLElement> implements Component {
   protected readonly element: T;
+
   constructor(htmlString: string) {
     const template = document.createElement('template');
     template.innerHTML = htmlString;
     this.element = template.content.firstElementChild! as T;
   }
+  
   attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
     parent.insertAdjacentElement(position, this.element);
   }
@@ -22,5 +25,9 @@ export class BaseComponent<T extends HTMLElement> implements Component {
       throw new Error('Parent mismatch!');
     }
     parent.removeChild(this.element);
+  }
+
+  attach(component: Component, position: InsertPosition = 'afterbegin') {
+    component.attachTo(this.element, position);
   }
 }
